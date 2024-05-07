087a837f5b6a4d11bb6671975190ad7e5311c080
Felix Körber <felixandrewkoerber@gmail.com>
2024-05-08 01:45:20 +0200

    w

---
+++ _site/README.md
@@ -0,0 +1,3 @@
+# ARENA Site
+
+This site is a fork of the [Allanlab](https://github.com/mpa139/allanlab) template.
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Equal Opportunities and Gender Equality Measures",
+        "excerpt":"   Career Development Measures for Female Researchers            Measures to Promote Diversity       Measures for Equal Opportunities within the Research Alliance Equal Opportunities Office, Goethe University Frankfurt, 2024       How female scientists can confront gender bias in the workplace          ","categories": [],
+        "tags": [],
+        "url": "/equal-opp/",
+        "teaser": "/assets/images/posts/1.jpg"
+      },{
+        "title": "Events and Training Opportunities",
+        "excerpt":"A European perspective on structural barriers to women`s career progression in neuroscience Date and Time: 07th May 2024, 12:00 - 14:00 Location: Seminar room 5G 170,PEG, Westend Campus, Goethe University Despite an unprecedented number of women entering neuroscience, and decades-long recruitment and retention efforts, women continue to be disproportionately underrepresented...","categories": [],
+        "tags": [],
+        "url": "/events/",
+        "teaser": "/assets/images/posts/3.jpg"
+      },{
+        "title": "Go Woman",
+        "excerpt":"Network for female* R3 scientists and newly appointed professors  ","categories": [],
+        "tags": [],
+        "url": "/go-woman/",
+        "teaser": "/assets/images/posts/2.jpg"
+      }]
